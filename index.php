9d7c46e9bec45610022d13ee1b995fedb71b449e
Roshan Adhikari <manjushreeroshan@gmail.com>
2022-03-13 15:02:56 -0800

    composer init and added core classes

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-	echo "Hello world";
+	require_once __DIR__.'/vendor/autoload.php';
+
+	use app\core\Application;
+
+	$app = new Application();
+
+	$router = new Router();
+
+	$app->router->get('/', function() {
+		return 'Hello world';
+	});
+
+	$app->run();
+
 ?>
