0bd83ea49290e3023ea97d76d36b0b694ecca2d3
Roshan Adhikari <manjushreeroshan@gmail.com>
2022-03-13 16:00:23 -0800

    added Request class

--- core/Router.php
+++ core/Router.php
@@ -3,19 +3,25 @@
 
   class Router
   {
+    public Request $request;
     protected array $routes = [];
+
+    public function __construct(Request $request)
+    {
+      $this->request = $request;
+    }
 
     public function get($path, $callback)
     {
       $this->routes['get'][$path] = $callback;
     }
 
+    //determine incoming request and resolve accordingly
     public function resolve()
     {
-      //determine incoming request and resolve accordingly
-      echo "<pre>";
-      var_dump($_SERVER);
-      echo "</pre>";
+      $path = $this->request->getPath();
+      $method = $this->request->getMethod();
+      echo $method;
     }
 
   }
