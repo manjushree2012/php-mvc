5decca6208d04aa654adfa821bd5ce3478c80488
Roshan Adhikari <manjushreeroshan@gmail.com>
2022-03-20 14:48:50 -0800

    registration validation using registration model

--- views/register.php
+++ views/register.php
@@ -29,7 +29,7 @@
                                         <div class="d-flex flex-row align-items-center mb-4">
                                             <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                                             <div class="form-outline flex-fill mb-0">
-                                                <input type="email" id="form3Example3c" class="form-control" name="email" />
+                                                <input type="text" id="form3Example3c" class="form-control" name="email" />
                                                 <label class="form-label" for="form3Example3c">Your Email</label>
                                             </div>
                                         </div>
@@ -45,22 +45,10 @@
                                         <div class="d-flex flex-row align-items-center mb-4">
                                             <i class="fas fa-key fa-lg me-3 fa-fw"></i>
                                             <div class="form-outline flex-fill mb-0">
-                                                <input type="password" id="form3Example4cd" class="form-control" name="confirmPassword"/>
+                                                <input type="password" id="form3Example4cd" class="form-control" name="passwordConfirm"/>
                                                 <label class="form-label" for="form3Example4cd">Repeat your password</label>
                                             </div>
                                         </div>
-
-                                        <!-- <div class="form-check d-flex justify-content-center mb-5">
-                                            <input
-                                            class="form-check-input me-2"
-                                            type="checkbox"
-                                            value=""
-                                            id="form2Example3c"
-                                            />
-                                            <label class="form-check-label" for="form2Example3">
-                                                I agree all statements in <a href="#!">Terms of service</a>
-                                            </label>
-                                        </div> -->
 
                                         <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
                                             <button type="submit" class="btn btn-primary btn-lg">Register</button>
