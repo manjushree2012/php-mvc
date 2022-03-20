bb6fe3f483171ead62a38ee8b26fc476a0fe22c8
Roshan Adhikari <manjushreeroshan@gmail.com>
2022-03-20 05:10:29 -0800

    added new routes for registration and login

--- public/index.php
+++ public/index.php
@@ -3,12 +3,18 @@
 
 	use app\core\Application;
 	use app\controllers\SiteController;
+	use app\controllers\AuthController;
 
 	$app = new Application(dirname(__DIR__));
 
 	$app->router->get('/home', 'home');
 	$app->router->get('/contact', [SiteController::class, 'contact']);
 	$app->router->post('/contact', [SiteController::class, 'handleContact']);
+
+	$app->router->get('/login', [AuthController::class, 'login']);
+	$app->router->post('/login', [AuthController::class, 'login']);
+	$app->router->get('/register', [AuthController::class, 'register']);
+	$app->router->post('/register', [AuthController::class, 'register']);
 
 	$app->run();
 
